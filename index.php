d7776c54fed63c738ee6c7516f492e7cd8fb6e49
kaho_inoue-PC\kaho_inoue <kahoinoue123@gmail.com>
2017-06-23 16:24:28 +0900

    石を置く処理：修正

--- index.php
+++ index.php
@@ -90,51 +90,53 @@ function getStonesByUserId($userId) {
     return json_decode($row['stone']);
   }
 }
-//そこに置くと相手の石が何個ひっくり返るかを返す
-//引数は現在の配置、行、列、石の色
+
+// そこに置くと相手の石が何個ひっくり返るかを返す
+// 引数は現在の配置、行、列、石の色
 function getFlipCountByPosAndColor($stones, $row, $col, $isWhite)
 {
   $total = 0;
-  //石川見た各方向への行、列の数の差
+  // 石から見た各方向への行、列の数の差
   $directions = [[-1, 0],[-1, 1],[0, 1],[1, 0],[1, 1],[1, 0],[1, -1],[0, -1],[-1, -1]];
-  //すべての方向をチェック
-  for ($i = 0; $i < count($directions); ++$i) {
-   // 置く場所からの距離。1つずつ進めながらチェックしていく
-   $cnt = 1;
-   // 行の距離
-   $rowDiff = $directions[$i][0];
-   // 列の距離
-   $colDiff = $directions[$i][1];
-   // はさめる可能性がある数
-   $flipCount = 0;
 
-   while (true) {
-     // 盤面の外に出たらループを抜ける
-     if (!isset($stones[$row + $rowDiff * $cnt]) || !isset($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt])) {
-       $flipCount = 0;
-       break;
-     }
-     // 相手の石なら$flipCountを加算
-     if ($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt] == ($isWhite ? 2 : 1)) {
-       $flipCount++;
-     }
-     // 自分の石ならループを抜ける
-     elseif ($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt] == ($isWhite ? 1 : 2)) {
-       break;
-     }
-     // どちらの石も置かれてなければループを抜ける
-     elseif ($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt] == 0) {
-       $flipCount = 0;
-       break;
-     }
-     // 1個進める
-     $cnt++;
-   }
-   // 加算
-   $total += $flipCount;
- }
- // ひっくり返る総数を返す
- return $total;
+  // 全ての方向をチェック
+  for ($i = 0; $i < count($directions); ++$i) {
+    // 置く場所からの距離。1つずつ進めながらチェックしていく
+    $cnt = 1;
+    // 行の距離
+    $rowDiff = $directions[$i][0];
+    // 列の距離
+    $colDiff = $directions[$i][1];
+    // はさめる可能性がある数
+    $flipCount = 0;
+
+    while (true) {
+      // 盤面の外に出たらループを抜ける
+      if (!isset($stones[$row + $rowDiff * $cnt]) || !isset($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt])) {
+        $flipCount = 0;
+        break;
+      }
+      // 相手の石なら$flipCountを加算
+      if ($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt] == ($isWhite ? 2 : 1)) {
+        $flipCount++;
+      }
+      // 自分の石ならループを抜ける
+      elseif ($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt] == ($isWhite ? 1 : 2)) {
+        break;
+      }
+      // どちらの石も置かれてなければループを抜ける
+      elseif ($stones[$row + $rowDiff * $cnt][$col + $colDiff * $cnt] == 0) {
+        $flipCount = 0;
+        break;
+      }
+      // 1個進める
+      $cnt++;
+    }
+    // 加算
+    $total += $flipCount;
+  }
+  // ひっくり返る総数を返す
+  return $total;
 }
 
 
