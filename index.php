067f19842401fc59a0181433e11fc94a02f6bc9f
kaho_inoue-PC\kaho_inoue <kahoinoue123@gmail.com>
2017-06-23 14:03:22 +0900

    dbConnecton

--- index.php
+++ index.php
@@ -208,4 +208,33 @@ function replyImagemap($bot, $replyToken, $alternativeText, $stones) {
   }
 }
 
+//データベースへの接続を管理するクラス
+class dbConnection {
+  //インスタンス
+  protected static $db;
+  //コンストラクタ
+  private function __construct(){
+
+    try {
+      //環境変数からデータベースへの接続情報を取得
+      $url = parse_url(getenv('DATEBASE_URL'));
+      //データソース
+      $dsn = sprintf('pgsql:host=%s;dbname=%s',$url['host'],substr($url['path'],1));
+      //接続を確立
+      self::$db = new PDO($dsn, $url['user'], $url['pass']);
+      //エラー時例外を投げるように設定
+      self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+    catch (PDOException $e){
+      echo 'Connection Error: ' . $e->getMessage();
+      }
+    //シングルトン。存在しない場合のみインスタンス化
+    public static function getConnection(){
+      if (!self::$db){
+        new dbConnection();
+        }
+        return self::$db;
+    }
+  }
+
 ?>
